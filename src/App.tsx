0835476ae53199910dfb70f115b7046560948cca
ryan teodoro <ryan.h.teodoro@gmail.com>
2025-02-06 22:26:15 -0600

    begin scaffolding ui for experiences and projects with rough data strutures

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ExternalLink } from "lucide-react";
 import Button from "./components/button";
 import Sidebar from "./components/sidebar";
 
@@ -55,9 +56,130 @@ function App() {
 
         <section id="experience" className="mb-20">
           <h2 className="text-2xl font-bold">experience</h2>
-          <p>here ar esome experiences</p>
-          <p>here ar esome experiences</p>
-          <p>here ar esome experiences</p>
+
+          {/* companies */}
+          {[
+            {
+              name: "4ccessible",
+              roles: [
+                {
+                  title: "Founder & CEO",
+                  description:
+                    "Lead and manage projects to make the internet accessible for the blind and visually impaired.",
+                  startDate: "April 2024",
+                  endDate: null,
+                  projects: [
+                    {
+                      title: "Browser Extension",
+                      description: "Leveraged gpt4o-mini to process guide ",
+                      languages: ["TypeScript"],
+                      frameworks: ["React"],
+                      databases: ["Postgres"],
+                      libraries: ["Redux Toolkit"],
+                      providers: ["DigitalOcean", "OpenAI", "Stripe"]
+                    },
+                    {
+                      title: "Website Scanner",
+                      description:
+                        "Developed a tool for companies to run ADA compliance scans",
+                      languages: ["TypeScript"],
+                      frameworks: ["React"],
+                      databases: ["Postgres"],
+                      libraries: ["axe-core", "Redux Toolkit"],
+                      providers: ["DigitalOcean"]
+                    }
+                  ]
+                }
+              ]
+            },
+            {
+              name: "Teodoro Systems",
+              roles: [
+                {
+                  title: "Founder & CEO",
+                  description:
+                    "Lead and manage software consulting projects across many different industries.",
+                  startDate: "November 2017",
+                  endDate: null,
+                  projects: [
+                    {
+                      title: "Stream for the Blind",
+                      description:
+                        "Created a streaming platform centered around accessible for the blind community.",
+                      url: "https://streamfortheblind.com",
+                      languages: ["TypeScript"],
+                      databases: ["Postgres", "Redis"],
+                      frameworks: ["React"],
+                      libraries: ["axe-core"],
+                      providers: [
+                        "AWS",
+                        "Supabase",
+                        "DigitalOcean",
+                        "Twilio",
+                        "Stripe"
+                      ]
+                    },
+                    {
+                      title: "Live Stream Real-Time Language Translator",
+                      description:
+                        "Created Python app to process live stream data via URL, convert audio to text, translate text from English to Spanish, then run final transform to convert text to speech. Built using fully open-source Vosk models and ran locally on edge hardware.",
+                      url: null,
+                      languages: ["Python"],
+                      databases: null,
+                      frameworks: [],
+                      libraries: ["vosk"],
+                      providers: []
+                    },
+                    {
+                      title: "EchoDial",
+                      description:
+                        "Developed dialer and CRM platform to automate",
+                      languages: ["TypeScript", "Python"],
+                      databases: ["Postgres"],
+                      frameworks: ["React"],
+                      libraries: ["axe-core"],
+                      providers: ["DigitalOcean", "Twilio", "Stripe"]
+                    }
+                  ]
+                }
+              ]
+            }
+          ].map((company) => (
+            <div className="border-2 my-4">
+              <div className="font-bold">{company.name}</div>
+
+              {/* roles within company */}
+              {company.roles.map((role) => (
+                <div>
+                  <div className="flex justify-between">
+                    <div>{role.title}</div>
+                    <div>
+                      {role.startDate} -{" "}
+                      {role.endDate ? role.endDate : "Present"}
+                    </div>
+                  </div>
+                  <div>{role.description}</div>
+
+                  {/* projects within role */}
+                  <div>
+                    {role.projects.map((project) => (
+                      <div className="pl-4 mb-2">
+                        <div className="font-semibold">{project.title}</div>
+                        <div>{project.description}</div>
+                        <div>
+                          {project.languages.map((language) => (
+                            <div className="text-white bg-teal-600 inline px-2 py-1 rounded-md text-sm mr-2">
+                              {language}
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ))}
 
           <Button
             title="see all experiences"
@@ -68,9 +190,52 @@ function App() {
 
         <section id="projects" className="mb-20">
           <h2 className="text-2xl font-bold">projects</h2>
-          <p>projecst!!!</p>
-          <p>projecst!!!</p>
-          <p>projecst!!!</p>
+          {[
+            {
+              title: "Portfolio Website",
+              description:
+                "React app for showcasing skills, experience, and projects.",
+              url: "https://ryanteodoro.com",
+              date: "February 2025",
+              skills: ["React", "TailwindCSS"],
+              platforms: ["Netlify"]
+            },
+            {
+              title: "Never Stolen",
+              description: "Laptop alarm app to prevent theft in coffee shops",
+              url: "https://ryanteodoro.com",
+              date: "February 2025",
+              skills: [
+                "React",
+                "Electron.js",
+                "Node.js",
+                "Socket.io (WebSockets)"
+              ],
+              platforms: ["DigitalOcean", "Twilio"]
+            }
+          ].map((project) => (
+            <div className="mb-4">
+              <div className="flex justify-between">
+                <div className="flex">
+                  <div className="font-semibold pr-1">{project.title}</div>
+                  <a href={project.url}>
+                    <ExternalLink />
+                  </a>
+                </div>
+
+                <div>{project.date}</div>
+              </div>
+
+              <div>{project.description}</div>
+              <div>
+                {project.skills.map((skill) => (
+                  <div className="text-white bg-teal-600 inline px-2 py-1 rounded-md text-sm mr-2">
+                    {skill}
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
 
           <Button title="see all projects" className="my-2" variant="outline" />
         </section>
