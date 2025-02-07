19aec2f6b73b50d7028acb0dcdb747b257a9e8fb
ryan teodoro <ryan.h.teodoro@gmail.com>
2025-02-07 01:31:13 -0600

    continue adding content to experiences/projects

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,10 @@ function App() {
             in the application of AI in the context of digital accessibility. My
             startup was accepted into the Tokyo startup centers at Google and
             Amazon and was recently announced as an Innovation Award finalist
-            for SXSW 2025.
+            for SXSW 2025. I also run{" "}
+            <a href="https://teodorosystems.com">
+              a small software consultancy.
+            </a>
           </p>
           <p className="mb-3"></p>
           <p className="mb-3">
@@ -73,20 +76,30 @@ function App() {
                       title: "Browser Extension",
                       description: "Leveraged gpt4o-mini to process guide ",
                       languages: ["TypeScript"],
-                      frameworks: ["React"],
+                      frameworks: ["Express.js"],
                       databases: ["Postgres"],
-                      libraries: ["Redux Toolkit"],
-                      providers: ["DigitalOcean", "OpenAI", "Stripe"]
+                      libraries: ["React", "Redux Toolkit"],
+                      providers: [
+                        "DigitalOcean",
+                        "Supabase",
+                        "OpenAI",
+                        "Stripe"
+                      ]
                     },
                     {
                       title: "Website Scanner",
                       description:
                         "Developed a tool for companies to run ADA compliance scans",
                       languages: ["TypeScript"],
-                      frameworks: ["React"],
+                      frameworks: ["Express.js"],
                       databases: ["Postgres"],
-                      libraries: ["axe-core", "Redux Toolkit"],
-                      providers: ["DigitalOcean"]
+                      libraries: [
+                        "React",
+                        "Redux Toolkit",
+                        "axe-core",
+                        "Cypress"
+                      ],
+                      providers: ["DigitalOcean", "Supabase"]
                     }
                   ]
                 }
@@ -109,8 +122,8 @@ function App() {
                       url: "https://streamfortheblind.com",
                       languages: ["TypeScript"],
                       databases: ["Postgres", "Redis"],
-                      frameworks: ["React"],
-                      libraries: ["axe-core"],
+                      frameworks: [],
+                      libraries: ["React", "ffmpeg"],
                       providers: [
                         "AWS",
                         "Supabase",
@@ -122,23 +135,32 @@ function App() {
                     {
                       title: "Live Stream Real-Time Language Translator",
                       description:
-                        "Created Python app to process live stream data via URL, convert audio to text, translate text from English to Spanish, then run final transform to convert text to speech. Built using fully open-source Vosk models and ran locally on edge hardware.",
+                        "Created Python app to process live stream data via URL, convert audio to text, translate text from English to Spanish, then run final transform to convert text to speech. Built using fully open-source Vosk models and ran locally on edge hardware. Also built a set of scripts to automate the retrieval of audio files from a website, transcribe them, then use a trained voice model to output summaries using AI to save staff time on mundane, reptitive tasks.",
                       url: null,
                       languages: ["Python"],
                       databases: null,
                       frameworks: [],
                       libraries: ["vosk"],
-                      providers: []
+                      providers: ["OpenAI", "ElevenLabs"]
                     },
                     {
                       title: "EchoDial",
                       description:
-                        "Developed dialer and CRM platform to automate",
-                      languages: ["TypeScript", "Python"],
+                        "Developed dialer and CRM platform to automate phone call outreach programs.",
+                      languages: ["TypeScript"],
                       databases: ["Postgres"],
-                      frameworks: ["React"],
-                      libraries: ["axe-core"],
+                      frameworks: ["Express.js"],
+                      libraries: ["React", "Mantine UI"],
                       providers: ["DigitalOcean", "Twilio", "Stripe"]
+                    },
+                    {
+                      title: "Perl Street (YC W22)",
+                      description: "Contributed to features",
+                      languages: ["TypeScript"],
+                      databases: [],
+                      frameworks: [],
+                      libraries: ["React", "Semantic UI"],
+                      providers: ["Google Cloud Platform"]
                     }
                   ]
                 }
@@ -201,10 +223,37 @@ function App() {
               platforms: ["Netlify"]
             },
             {
+              title: "Fern (YC W23)",
+              description:
+                "Contributed to open source library for the Fern API.",
+              url: "https://github.com/fern-api/fern",
+              date: "January 2025",
+              skills: ["TypeScript", "Node.js"],
+              platforms: ["Fern"]
+            },
+            {
+              title: "mOcKcAsE",
+              description: "Created NPM library for string formatting",
+              url: "https://www.npmjs.com/package/mockcase",
+              date: "December 2024",
+              skills: ["TypeScript", "Node.js"],
+              platforms: ["NPM"]
+            },
+            {
+              title: "National Cortado Day",
+              description:
+                "Created national food holiday for one of my favorite espresso drinks.",
+              url: "https://www.nationalcortadoday.com",
+              date: "December 2021",
+              skills: ["HTML", "CSS"],
+              platforms: ["Wikipedia"]
+            },
+            {
               title: "Never Stolen",
-              description: "Laptop alarm app to prevent theft in coffee shops",
-              url: "https://ryanteodoro.com",
-              date: "February 2025",
+              description:
+                "Alarm app to prevent thieves from stealing your MacBook.",
+              url: "https://neverstolen.ryanteodoro.com",
+              date: "March 2020",
               skills: [
                 "React",
                 "Electron.js",
